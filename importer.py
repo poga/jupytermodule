fd27cdc1d1f3f58f72104c804efb21d743bdeb31
Poga Po <poga.bahamut@gmail.com>
2020-01-08 16:58:55 +0800

    update

--- importer.py
+++ importer.py
@@ -1,41 +1,45 @@
 import sys
-import types  # Used to instantiate a Module object, unavailable as a builtin
+import types
 import os.path
+from os.path import join
+import re
+# from pprint import pprint
+from urllib.request import urlopen
+import nbformat
+from nbconvert import PythonExporter
+import ast
 
-# os.path.join is used frequently, so we shorten our invocation
-from os.path import join as ospj
+from analyzer import Analyzer
 
 
-class TmpFinder(object):
-    """ Class to find and load modules from `/tmp/modules/` """
-    def __init__(self):
-        self.greeting = 'Hello from TmpFinder'
-        self.tmp_prefix = ospj('/tmp', 'modules')
+class NbFinder(object):
+    def try_url(self, url_without_extension):
+        url = url_without_extension
+        # print(url)
+        try:
+            resp = urlopen(url)
+
+            if resp.getcode() == 200:
+                return url
+        except Exception as e:
+            # try again with .ipynb extension
+            url += '.ipynb'
+            # print(url)
+            try:
+                resp = urlopen(url)
+
+                if resp.getcode() == 200:
+                    return url
+            except Exception as e:
+                return None
 
     def find_module(self, fullname, path=None):
-        # Adding print statements, so that (1) we know everytime find_module is
-        # invoked during the import process and (2) we see what the name and
-        # path of the module passed to us look like.
-        print(fullname, path)
-
-        # NOTE: For simplicity and brevity, we assume the module we're being
-        # given is a top-level file module, not a directory based package or
-        # sub-package. We also ignore `path`, doing so will break subpackage
-        # imports and relative imports. Adding this functionality is easy
-        # enough, but it detracts from the example, so we leave it out.
-        location = ospj(self.tmp_prefix, *(fullname.split('.')))
-
-        if os.path.exists(location):
-            # We're returning the loader object here, which in this case, just
-            # happens to be the same as the finder object
+        if self.try_url(fullname):
             return self
 
-        # Default return for a function is None of course, so we do nothing
-        # special when we don't find the module
-
     def load_module(self, fullname):
-        # Helpful print statement tells us when loader is used
-
+        url = self.try_url(fullname)
+        # print("loading", url)
         # If the module already exists in `sys.modules` we *must* use that
         # module, it's a mandatory part of the importer protcol
         if fullname in sys.modules:
@@ -44,25 +48,37 @@ class TmpFinder(object):
             # we skip this part.
             return
 
-        location = ospj(self.tmp_prefix, *(fullname.split('.')))
-
         try:
+            m = types.ModuleType(fullname,
+                                 'remote notebook: {}'.format(fullname))
+            m.__file__ = '<notebook {}>'.format(fullname)
+            m.__name__ = url
+            m.__loader__ = self
+            sys.modules[url] = m
+
             # The importer protocol requires the loader create a new module
             # object, set certain attributes on it, then add it to
             # `sys.modules` before executing the code inside the module (which
             # is when the "module" actually gets code inside it)
+            resp = urlopen(url)
 
-            m = types.ModuleType(fullname,
-                                 'This is the doc string for the module')
-            m.__file__ = '<tmp {}>'.format(location)
-            m.__name__ = fullname
-            m.__loader__ = self
-            sys.modules[fullname] = m
+            body = resp.read().decode()
+            notebook = nbformat.reads(body, as_version=4)
+            exporter = PythonExporter()
+            (code, resources) = exporter.from_notebook_node(notebook)
+            code = re.sub(r'%.+', "", code)
+
+            tree = ast.parse(code)
+            analyzer = Analyzer()
+            analyzer.visit(tree)
+            # print(analyzer.code())
 
             # Attempt to open the file, and exec the code therein within the
             # newly created module's namespace
-            with open(location, 'r') as f:
-                exec(f) in sys.modules[fullname].__dict__
+            # with open(location, 'r') as f:
+            # print("execing", analyzer.code(), url)
+            exec(analyzer.code(), sys.modules[url].__dict__)
+            sys.modules[fullname] = sys.modules[url]
 
             # Return our newly create module
             return m
@@ -75,9 +91,21 @@ class TmpFinder(object):
 
             # Delete the entry we might've created; use LBYL to avoid nested
             # exception handling
+            if sys.modules.get(url):
+                del sys.modules[url]
+            raise e
+
             if sys.modules.get(fullname):
                 del sys.modules[fullname]
             raise e
 
 
-sys.meta_path.append(TmpFinder())
+from importlib import import_module
+
+
+# url = "http://localhost:8000/Untitled.ipynb"
+def nbimport(url):
+    return import_module(url)
+
+
+sys.meta_path.append(NbFinder())
--- main.py
+++ main.py
@@ -1,61 +1,6 @@
-from urllib.request import urlopen
-import ast
-from pprint import pprint
-import re
-import astor
-
-url = 'http://jakevdp.github.com/downloads/notebooks/XKCD_plots.ipynb'
-response = urlopen(url).read().decode()
-# print(response)
-
-import nbformat
-jake_notebook = nbformat.reads(response, as_version=4)
-# print(jake_notebook.cells[9].source)
-# print(jake_notebook.cells[12])
-
-# 1. Import the exporter
-from nbconvert import PythonExporter
-
-# 2. Instantiate the exporter. We use the `basic` template for now; we'll get into more details
-# later about how to customize the exporter further.
-export = PythonExporter()
-
-# 3. Process the notebook we loaded earlier
-(body, resources) = export.from_notebook_node(jake_notebook)
-# print(body)
-
-
-class Analyzer(ast.NodeVisitor):
-    def __init__(self):
-        self.stats = {"import": [], "from": []}
-
-    def visit_Import(self, node):
-        for alias in node.names:
-            self.stats["import"].append(alias.name)
-        self.generic_visit(node)
-
-    def visit_ImportFrom(self, node):
-        for alias in node.names:
-            self.stats["from"].append(alias.name)
-        self.generic_visit(node)
-
-    def visit_FunctionDef(self, node):
-        print(node.name, astor.to_source(node))
-
-    def visit_ClassDef(self, node):
-        print(node.name, astor.to_source(node))
-
-    def report(self):
-        pprint(self.stats)
-
-
-py = re.sub(r'%.+', "", body)
-# print(py)
-tree = ast.parse(py)
-analyzer = Analyzer()
-analyzer.visit(tree)
-analyzer.report()
-
 # import importer
-# import spam
-# spam.foo()
+from importer import nbimport
+
+url = "http://localhost:8000/Untitled.ipynb"
+m = nbimport(url)
+print(m.primes(100))
