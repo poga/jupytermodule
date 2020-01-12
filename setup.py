bd3bd8689b70f299de2ff151b3f0af9ffc969e12
Poga Po <poga.bahamut@gmail.com>
2020-01-12 21:33:15 +0800

    rename repo

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="jupytermodule",  # Replace with your own username
-    version="0.3.0",
+    version="0.4.0",
     author="Poga",
     author_email="poga.po@gmail.com",
     description="import jupyter notebooks as python modules",
